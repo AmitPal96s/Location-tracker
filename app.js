b0e7c28676d4083b5843eb3e968290b56e05aeb6
Amit Pal <146516790+AmitPal96s@users.noreply.github.com>
2026-01-30 19:57:41 +0530

    add start script for render

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ app.get('/', (req, res) => {
 });
 
 // IMPORTANT: listen on server, not app
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
